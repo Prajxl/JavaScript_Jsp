a179c5d9e3958b8111996c75fc6a2d9b4cb5cb48
Prajwal K <154422749+Prajxl@users.noreply.github.com>
2026-05-28 18:00:44 +0530

    Input using prompt sync

--- LogicalOp.js
+++ LogicalOp.js
@@ -6,4 +6,11 @@ print = console.log.bind();
 let userName = "user111"
 let password = "user@111"
 
-userName === "user111" && password === "user@111" ? "Welcome to Home Page" : " Please Check username and password"
+const auth = userName === "user111" && password === "user@111" ? "Welcome to Home Page" : " Please Check username and password"
+print(auth)
+
+// Taking Input using User - Actually using Html but now if we dont have html
+
+const prompt = require("prompt-sync")({sigint:true})
+let userInput =prompt("Enter your input :")
+print(userInput)
